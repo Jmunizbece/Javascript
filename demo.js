3d066b44647d6313be84d570c10d6fa71ad25df9
Jmunizbece <Jmunizbece@users.noreply.github.com>
2021-02-24 10:45:01 -0500

    DOM and Use of JS file within a HTML document and Providing  Source of JS File
    Learned of the importance of the document object model as it allows to make sense of the hierarchy of the HTML document and as a result be able to manipulate the attributes of the objects involved. Lastly, I was able to learn of how to either write the js script within the HTML document or include the name of the js source file.

---
+++ demo.js
@@ -0,0 +1,13 @@
+// Introduction ------------------------------------------------
+/*
+Javascript is very useful in adding programmming to web pages and allowing for functionality within those webpages. Javascript works alongside HTML and CSS and those provide structure and styling respectively.
+
+Understanding the document object model 'DOM' we are able to understand how JS is able to work with HTML and CSS.
+
+DOM is just another representation of the HTML hierarchy that JS is capable of understanding. Since JS is able to understand the hierarchy of the HTML document now, it is capable of manipulating the attributes of these objects. 
+
+There are element, attribute and text nodes that JS is capable of manipulating
+*/
+
+// How to use external js file to include in html
+document.write("External File."); 
